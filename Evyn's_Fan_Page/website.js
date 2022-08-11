9ed3c6757b1fd20465569e25df239f31ec6e0a6b
RyanWiegman <ryanwiegman@gmail.com>
2022-08-10 20:04:35 -0400

    started

---
+++ Evyn's_Fan_Page/website.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// const switcher = document.querySelector('.btn');
+
+// switcher.addEventListener('click', () => {
+//     document.body.classList.toggle('light-theme');
+//     document.body.classList.toggle('dark-theme');
+
+//     const className = document.body.className;
+//     if(className == "light-theme")
+//         this.textContent = "Dark";
+//     else
+//         this.textContent = "Light";
+
+//     console.log('current class name: ' + className);
+
+// });
+
+function colorChange() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+    
+    const Name = document.body.className;
+    if(Name == "light-theme")
+        this.textContent = "Dark";
+    else
+        this.textContent = "Light";
+}
